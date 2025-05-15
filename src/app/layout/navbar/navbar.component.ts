488b60d0e58bafe285cb3bead9bf6e01046b5bfc
victor <victor.torreblanca.franco@vallegrande.edu.pe>
2025-05-14 21:34:34 -0500

    feat(navbar): update navbar layout and add logo to header

--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
+  imports: [MatButtonModule, MatIconModule],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  onLogin() {
+    console.log('Login button clicked');
+  }
+}
